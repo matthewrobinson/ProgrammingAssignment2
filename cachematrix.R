b67612ae2c6eb1ae740664b8a9936578c3051c35
Matthew Robinson <matthewrobinson@users.noreply.github.com>
2014-10-23 21:04:19 +0100

    added makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix (R Programming assignment 2)
+## 
+## Solution by Matthew Robinson
+##
+## Code below is my own work, but is based heavily on the example
+## makeVector/cachemean functions given in the assignment
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special "CacheMatrix" which allows you to get/set:
+##	  the value of the matrix
+##      a cached value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        inverse <- NULL # initialise cache
 
+        set <- function(value) {
+                x <<- value
+                inverse <<- NULL   # value changed, so cache no longer valid
+        }
+
+        get <- function() x
+
+        setSolve <- function(solution) inverse <<- solution
+
+        getSolve <- function() inverse
+
+        # return a list of functions which can be used to 
+        # manipulate the CacheMatrix
+        list(set = set, get = get,
+             setSolve = setSolve,
+             getSolve = getSolve)
 }
-
 
 ## Write a short comment describing this function
 
